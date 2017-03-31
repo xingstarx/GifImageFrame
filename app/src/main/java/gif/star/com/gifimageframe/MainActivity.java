dc866cd9e36a1ac05c4cb3db90ee18ca9c724220
xingxing <xxx823952375@gmail.com>
2017-03-31 22:26:34 +0800

    首次提交，采animation-list的方式实现gif效果

---
+++ app/src/main/java/gif/star/com/gifimageframe/MainActivity.java
@@ -0,0 +1,19 @@
+package gif.star.com.gifimageframe;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        ((AnimationDrawable) mImageView.getDrawable()).start();
+    }
+}
