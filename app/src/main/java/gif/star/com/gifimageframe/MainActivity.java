7b0d3df5c4f5452625f04a996ffebd2cf76ff740
xingxing <xxx823952375@gmail.com>
2017-03-31 23:24:28 +0800

    add code

--- app/src/main/java/gif/star/com/gifimageframe/MainActivity.java
+++ app/src/main/java/gif/star/com/gifimageframe/MainActivity.java
@@ -2,18 +2,32 @@ package gif.star.com.gifimageframe;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MSG_SHOW = 1;
     private ImageView mImageView;
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SHOW:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mImageView = (ImageView) findViewById(R.id.image_view);
-        ((AnimationDrawable) mImageView.getDrawable()).start();
+
     }
+
+
 }
